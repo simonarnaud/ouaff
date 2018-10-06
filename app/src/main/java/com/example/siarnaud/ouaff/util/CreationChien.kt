efc18f0f50f51d9d5755de01038e8512a92bebea
Simon <21604666>
2018-10-06 12:48:33 +0200

    test commitb2

---
+++ app/src/main/java/com/example/siarnaud/ouaff/util/CreationChien.kt
@@ -0,0 +1,9 @@
+package com.example.siarnaud.ouaff.util
+
+class CreationChien {
+    companion object {
+        /*fun createDog(dogName: String, dogRace: String, dogGender) {
+
+        }*/
+    }
+}
